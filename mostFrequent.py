42373e9e5aa5f4afccd47f8797de4a94f2abd3be
Ezracancoj <102781998+Ezracancoj@users.noreply.github.com>
2022-04-18 12:52:13 -0400

    Add files via upload

---
+++ mostFrequent.py
@@ -0,0 +1,25 @@
+# mostFrequent
+# CS 175
+# Ez Racancoj
+def main():
+    List = []
+    uinput = input('Enter a string: ')
+    for x in uinput:
+        if x != ' ':
+            List.append(x)
+    
+    most_frequent(List)
+
+    print(most_frequent(List))
+    
+    
+    
+def most_frequent(List):
+    return max(set(List), key = List.count)
+
+
+
+
+
+
+main()
